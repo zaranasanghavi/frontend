86f3b2261217eb6f268349f701b0a51078563b8a
zaranasanghavi <157245898+zaranasanghavi@users.noreply.github.com>
2025-05-04 18:35:25 +0530

    Update api.jsx

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",  // Make sure this is correct
+  baseURL: "https://backend-3uy2.onrender.com/api",
   headers: { "Content-Type": "application/json" }
 });
 
